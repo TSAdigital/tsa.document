b6b49de392d67cb64727a987a9a32ba9ef79c6fe
TSAdigital <s.tarhanov174@gmail.com>
2022-12-10 21:40:52 +0500

    version 1.1.0

--- web/js/tab.js
+++ web/js/tab.js
@@ -1,10 +1,10 @@
-$(function($){
+$('#myTab').ready(function(){
     var storage = localStorage.getItem('nav-item');
     if (storage && storage !== "#") {
         $('.nav-item a[href="' + storage + '"]').tab('show');
     }
 
-    $('ul.nav li').on('click', function() {
+    $('#myTab.nav li').on('click', function() {
         var id = $(this).find('a').attr('href');
         localStorage.setItem('nav-item', id);
     });
